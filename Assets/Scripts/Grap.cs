ac96e368f322b2cd2ed2d244b929c05c450b9447
Ahmed Bellarbi <Ahmedbellarbi@gmail.com>
2022-01-11 13:50:10 +0100

    Shooting Scene

--- Assets/Scripts/Grap.cs
+++ Assets/Scripts/Grap.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,44 +7,53 @@ public class Grap : MonoBehaviour
 {
    // private GameObject cube;
     public GameObject Hands;
-    public GameObject cubes;
+    public GameObject Weapons;
     bool CanGrap = true;
     bool inHands = false;
     Vector3 ballPos;
+    Quaternion wRotation;
+ 
+    //string name = "none";
     // Start is called before the first frame update
     void Start()
     {
-       
+        
         ballPos = this.transform.position;
+        wRotation = this.transform.rotation;
         // Update is called once per frame
     }
     void Update()
         {
 
-     
-
+      
+         
         if (Input.GetButtonDown("Fire1"))
         {
-            
-                if (!inHands)
+           
+                if (!inHands )
+              
                 {
+               
                     this.transform.SetParent(Hands.transform);
-                    this.transform.localPosition = new Vector3(-0.05f, -.5f, 0.87f);
+                    this.transform.localPosition = new Vector3(0.173f, -.293f, 0.278f);
+                this.transform.rotation = Quaternion.Euler(10,90,0);
                     inHands = true;
-
+                
 
                 }
 
-                else if (inHands)
+                else if (inHands  )
                 {
                     this.GetComponent<Grap>().enabled = false;
-                    this.transform.SetParent(cubes.transform);
+                    this.transform.SetParent(Weapons.transform);
                     this.transform.localPosition = ballPos;
+                this.transform.rotation = wRotation;
 
                     inHands = false;
                 }
             }
-        
+
+
         if (this.transform.parent == Hands.transform)
         {
             GetComponent<Renderer>().material.color = Color.cyan;
---
+++ Assets/Scripts/Grap1.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grap1 : MonoBehaviour
+{
+    // private GameObject cube;
+    public GameObject Hands;
+    public GameObject cubes;
+    public GameObject[] cube;
+    bool inHands = false;
+    Vector3 ballPos;
+    public GvrBasePointer ray;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (cube == null)  
+        cube = GameObject.FindGameObjectsWithTag("Cube");
+        ballPos = this.transform.position;
+       
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            foreach (GameObject cu in cube)
+            {
+                if (!inHands)
+                {
+
+                    cu.transform.SetParent(Hands.transform);
+                    cu.transform.localPosition = new Vector3(-0.05f, -.5f, 0.87f);
+                    inHands = true;
+
+
+                }
+
+                else if (inHands)
+                {
+                    this.GetComponent<Grap>().enabled = false;
+                    cu.transform.SetParent(cubes.transform);
+                    cu.transform.localPosition = ballPos;
+
+                    inHands = false;
+                }
+            }
+        }
+        Debug.Log(Hands.transform.childCount);
+
+
+        if (this.transform.parent == Hands.transform)
+        {
+            GetComponent<Renderer>().material.color = Color.cyan;
+        }
+        else
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+    }
+}
